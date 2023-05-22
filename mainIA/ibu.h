7cb2e0e899d9ac991312c3dae200713ef654e834
ikhdaamel <ikhda.new45@gmail.com>
2023-05-22 17:55:26 +0700

    membuat void

--- mainIA/ibu.h
+++ mainIA/ibu.h
@@ -15,3 +15,15 @@ public:
 	void tambahAnak(anak*);
 	void cetakAnak();
 };
+
+void ibu::tambahAnak(anak* pAnak) {
+	daftar_anak.push_back(pAnak);
+}
+void ibu::cetakAnak() {
+	cout << "daftar anak dari ibu \"" << this->nama << "\":\n";
+	for (auto& a : daftar_anak) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+#endif
