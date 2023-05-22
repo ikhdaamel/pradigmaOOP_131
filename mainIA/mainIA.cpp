49a477aa9ba7895f8d1a19eb08bf1af39175969f
ikhdaamel <ikhda.new45@gmail.com>
2023-05-22 17:50:39 +0700

    membuat class ibu

---
+++ mainIA/mainIA.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include "ibu.h"
+#include "anak.h"
+
+int main() {
+	ibu* varIbu = new ibu("dini");
+	ibu* varIbu2 = new ibu("novi");
+	anak* varAnak1 = new anak("tono");
+}
