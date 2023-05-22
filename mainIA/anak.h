16a7d6e4d42027c8186b9314774cf32ccfd8cc0c
ikhdaamel <ikhda.new45@gmail.com>
2023-05-22 17:59:49 +0700

    membuat class anak

--- mainIA/anak.h
+++ mainIA/anak.h
@@ -1,1 +1,14 @@
-#pragma once
+#ifndef ANAK_H
+#define ANAk_H
+
+class anak {
+public:
+	string nama;
+	anak(string pNama) :nama(pNama) {
+		cout << "anak \"" << nama << "\" ada\n";
+	}
+	~anak() {
+		cout << "anak \"" << nama << "\" tidak ada\n";
+	}
+};
+3endif
